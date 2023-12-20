11b20ddadfc592f6e803ec5d116117fc92871ae5
amayadf <106173666+amayadf@users.noreply.github.com>
2023-12-20 13:18:17 -0500

    Add event listeners to createBookCard()

--- scripts.js
+++ scripts.js
@@ -41,5 +41,64 @@ class Library {
 
 const library = new Library();
 
-//User interface 
+//Obtaining html elements
+let addBookBtn = document.querySelector('.add-book-btn');
+let booksGrid = document.querySelector('.books-grid');
 
+
+
+//Book grid functions
+function createBookCard(newBook) {
+    //creating card elements
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    //adding their classes
+    bookCard.classList.add('book-card');
+    bookTitle.classList.add('book-title');
+    bookAuthor.classList.add('book-author');
+    bookPages.classList.add('book-pages');
+    removeBtn.classList.add('remove-book-btn');
+
+    //add event handlers to buttons
+    readBtn.addEventListener('click', toogleRead);
+    removeBtn.addEventListener('click', removeBook)
+
+    //adding their content
+    bookTitle.textContent = `"${newBook.title}"`;
+    bookAuthor.textContent = newBook.author;
+    bookPages.textContent = `${newBook.pages} pages`;
+    removeBtn.textContent = 'Remove';
+
+    if(newBook.isRead) {
+        readBtn.textContent = 'Read';
+        readBtn.classList.add('read');
+    }
+    else {
+        readBtn.textContent = 'Not read';
+        readBtn.textContent.add('not-read');
+    }
+
+    //adding elements as children of book card
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(removeBtn);
+}
+
+
+function createBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('is-read').checked;
+    return new Book(title, author, pages, isRead);
+}
+
+
+//Event listeners
